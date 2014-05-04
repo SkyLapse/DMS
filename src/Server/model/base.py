28767b1c242198f69047888b03367bc4b13e288c
Leo Bernard <admin@leolabs.org>
2014-05-04 23:15:36 +0200

    Added better logging

--- src/Server/model/base.py
+++ src/Server/model/base.py
@@ -2,23 +2,28 @@ import os
 
 __author__ = 'leobernard'
 
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 import json
 from model import *
 
 
 class Base():
-    def __init__(self, config_path="../config/db.json"):
+    def __init__(self, logger, config_path="../config/db.json"):
 
         with open(os.path.dirname(__file__) + "/" + config_path) as data_file:
             data = json.load(data_file)
 
-        self.config = data
-        self.client = MongoClient(data['host'], data['port'], data['maxPoolSize'])
-        self.db = self.client[data['database']]
-
-        self.documents = Documents(self, self.config, self.client, self.db)
-        self.categories = Categories(self, self.config, self.client, self.db)
-        self.tags = Tags(self, self.config, self.client, self.db)
-        self.users = Users(self, self.config, self.client, self.db)
-        self.appkeys = Appkeys(self, self.config, self.client, self.db)
+        try:
+            self.config = data
+            self.client = MongoClient(data['host'], data['port'], data['maxPoolSize'])
+            self.db = self.client[data['database']]
+        except errors.ConnectionFailure:
+            logger.critical("Could not connect to the MongoDB server at %s:%s" %
+                            (data['host'], data['port']))
+            exit(1)
+        else:
+            self.documents = Documents(self, self.config, self.client, self.db)
+            self.categories = Categories(self, self.config, self.client, self.db)
+            self.tags = Tags(self, self.config, self.client, self.db)
+            self.users = Users(self, self.config, self.client, self.db)
+            self.appkeys = Appkeys(self, self.config, self.client, self.db)
