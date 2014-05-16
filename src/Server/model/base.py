763fd50ed71b7f1d5e2305dc6343823952326fe6
Moritz Gunz <moritz.gunz@gmail.com>
2014-05-16 17:59:11 +0200

    Changed .gitignore

--- src/Server/model/base.py
+++ src/Server/model/base.py
@@ -10,16 +10,16 @@ from model import *
 class Base():
     def __init__(self, logger, config_path="../config/db.json"):
 
-        with open(os.path.dirname(__file__) + "/" + config_path) as data_file:
-            data = json.load(data_file)
+        with open(os.path.join(os.path.dirname(__file__), config_path)) as config_file:
+            config = json.load(config_file)
 
         try:
-            self.config = data
-            self.client = MongoClient(data['host'], data['port'], data['maxPoolSize'])
-            self.db = self.client[data['database']]
+            self.config = config
+            self.client = MongoClient(config['host'], config['port'], config['maxPoolSize'])
+            self.db = self.client[config['database']]
         except errors.ConnectionFailure:
             logger.critical("Could not connect to the MongoDB server at %s:%s" %
-                            (data['host'], data['port']))
+                            (config['host'], config['port']))
             exit(1)
         else:
             self.documents = Documents(self, self.config, self.client, self.db)
--- src/Server/workers/baseworker.py
+++ src/Server/workers/baseworker.py
@@ -7,8 +7,8 @@ import os
 
 class BaseWorker():
     def __init__(self, config_path="../config/workers.json"):
-        with open(os.path.dirname(__file__) + "/" + config_path) as data_file:
-            data = json.load(data_file)
+        with open(os.path.join(os.path.dirname(__file__), config_path)) as config_file:
+            config = json.load(config_file)
 
-        self.pool = multiprocessing.Pool(multiprocessing.cpu_count() * data['maxWorkersPerCore'])
-        self.config = data
+        self.pool = multiprocessing.Pool(multiprocessing.cpu_count() * config['maxWorkersPerCore'])
+        self.config = config
